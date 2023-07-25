7553a67249d812063b26526ed4a8e243b17ecbcb
Oluwasegun Akinpelu <akinpelu53@gmail.com>
2023-07-25 12:45:18 +0100

    ..

--- index.js
+++ index.js
@@ -2,12 +2,39 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Oluwasegun:nvidia@cluster0.jgruyhl.mongodb.net/?retryWrites=true&w=majority";
+
+
 // Express APIs
 const api = require("./Routes/auth.routes");
 // Connecting MongoDB
+/*
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("test").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}run().catch(console.dir);
+*/
 async function mongoDbConnection() {
   await mongoose.connect(
-    "mongodb://127.0.0.1:27017/test",
+    "mongodb+srv://Oluwasegun:nvidia@cluster0.jgruyhl.mongodb.net/test",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
